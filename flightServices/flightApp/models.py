a65f7f91700b134886104cd3572321e53fd3b25b
online5880 <gjtjqkr1@naver.com>
2024-11-14 01:06:51 +0900

    AuthToken, Angular

--- flightServices/flightApp/models.py
+++ flightServices/flightApp/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.conf import settings
+
 # Create your models here.
 class Flight(models.Model):
     flightNumber = models.CharField(max_length=10)      # 항공기 번호
@@ -21,4 +26,10 @@ class Reservation(models.Model):
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
     passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
     
-    
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def createAuthToken(sender, instance, created, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        
